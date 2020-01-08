b3d650da69df5d70231e9581dff8f796b88954da
Keegan Mullaney <keegan@kmauthorized.com>
2020-01-07 23:24:08 -0800

    Move listen function to server.js
    Define app.use functions.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const todoRoutes = require('./routes/todo.routes');
+const app = express();
+const mongodb = require('./mongodb/mongodb.connect');
+
+mongodb.connect();
+
+app.use(express.json());
+app.use('/todos/', todoRoutes);
+app.use((error, req, res, next) => {
+  // console.log(error);
+  res.status(500).json({ message: error.message });
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+  res.json('Hello world!');
+});
 
-app.listen(3000, () => {
-    console.log('Server is now running!')
-})
-  
+module.exports = app;
+/**
+ CRUD:
+   Create: HTTP POST
+   Read: HTTP GET
+   Update: HTTP PUT
+   Delete: HTTP DELETE
+ */

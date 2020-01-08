f2786d6eb787d8091407265d4b93fecca0f686bd
Keegan Mullaney <keegan@kmauthorized.com>
2020-01-07 23:22:12 -0800

    add newly created code
    Creating a REST API with mongodb and tests with Jest.
    Includes: model, controller, routes, server, and mock data.

---
+++ test/integration/todo.controller.int.test.js
@@ -0,0 +1,35 @@
+const request = require('supertest');
+const app = require('../../app');
+const newTodo = require('../mock-data/new-todo.json');
+
+const endpointUrl = '/todos/';
+
+describe(endpointUrl, () => {
+  // integration tests for HTTP GET
+  test('GET ' + endpointUrl, async () => {
+    const response = await request(app).get(endpointUrl);
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+    expect(response.body[0].title).toBeDefined();
+    expect(response.body[0].done).toBeDefined();
+  });
+  // integration tests for HTTP POST
+  it('POST' + endpointUrl, async () => {
+    const response = await request(app)
+      .post(endpointUrl)
+      .send(newTodo);
+    expect(response.statusCode).toBe(201);
+    expect(response.body.title).toBe(newTodo.title);
+    expect(response.body.done).toBe(newTodo.done);
+  });
+  // error handling for HTTP POST
+  it('should return error 500 on malformed data with POST' + endpointUrl, async () => {
+    const response = await request(app)
+      .post(endpointUrl)
+      .send({ title: 'Missing done property' });
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toStrictEqual({
+      message: 'Todo validation failed: done: Path `done` is required.'
+    });
+  });
+});
